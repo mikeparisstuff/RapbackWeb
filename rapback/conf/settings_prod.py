de67e06c53b318f4f506b8127fecf0dba49fcbcf
Michael Paris <mlparis92@gmail.com>
2014-09-04 00:49:13 -0400

    Dbupdate

--- rapback/conf/settings_prod.py
+++ rapback/conf/settings_prod.py
@@ -7,7 +7,7 @@ DATABASES = {
             # The following settings are not used with sqlite3:
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': 'ec2-54-165-7-42.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+            'HOST': 'rapbackprod.cp59mlmcctc3.us-east-1.rds.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
             'PORT': os.environ['RDS_PORT'],                      # Set to empty string for default.
     }
 }
